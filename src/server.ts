6a2b16cdb9dae20ab59f1da33424f5b82674f0ba
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-03-14 23:34:40 +0600

    user table ad admin table created

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
-import express from 'express';
 
-const app = express();
+import { Server } from 'http';
+import app from './app';
+
 const port=5000;
 
-app.listen(port,() => {
-    console.log("App Listening on port",port);
-})
+async function main(){
+    const server:Server = app.listen(port,() => {
+        console.log("Server Is Running On Port",port);
+    })
+}
+main();
