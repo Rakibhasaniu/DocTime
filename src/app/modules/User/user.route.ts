7319eda86b76b478ec84bd36153eebd51c71843f
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-03-27 22:17:13 +0600

    create admin using role

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -8,24 +8,24 @@ import auth from "../../middleware/auth";
 
 const router = Router();
 
-const auth = (...roles:string[]) => {
-    return async(req:Request,res:Response,next:NextFunction) => {
-        try {
-            const token = req.headers.authorization
-            if(!token){
-                throw new Error("You are not authorized");
-            }
-            const verifiedUser = decodedToken.verifyToken(token,config.jwt.jwt_access_key as Secret)
-            console.log(verifiedUser)
-            if(roles.length && !roles.includes(verifiedUser.role)){
-                throw new Error("You are not authorized");
-            }
-            next()
-        } catch (err) {
-            next(err);
-        }
-    }
-}
+// const auth = (...roles:string[]) => {
+//     return async(req:Request,res:Response,next:NextFunction) => {
+//         try {
+//             const token = req.headers.authorization
+//             if(!token){
+//                 throw new Error("You are not authorized");
+//             }
+//             const verifiedUser = decodedToken.verifyToken(token,config.jwt.jwt_access_key as Secret)
+//             console.log(verifiedUser)
+//             if(roles.length && !roles.includes(verifiedUser.role)){
+//                 throw new Error("You are not authorized");
+//             }
+//             next()
+//         } catch (err) {
+//             next(err);
+//         }
+//     }
+// }
 
 router.post('/create-admin',auth("ADMIN","SUPER_ADMIN"),userController.createAdmin)
 
