c37de7c6de93b0d95df7f20c06fcbd33f3ef7912
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-04-04 23:31:05 +0600

    handler change

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,3 +1,43 @@
+// import { ErrorRequestHandler, NextFunction, Request, Response } from "express"
+// import httpStatus from "http-status"
+// import { ZodError, ZodIssue, z } from "zod";
+// import handleZodError from "../errors/handleZodError";
+
+
+// const globalErrorHandler:ErrorRequestHandler = (err, req, res, next) => {
+    
+//     let statusCode = err.status ||  500;
+//     let message = err.message || 'Something went wrong';
+    
+//     // let errorDetails = {
+//     //     zodError:[ {
+//     //         // field:'',
+//     //         message:'something went wrong'
+//     //     }]
+//     // }
+   
+    
+    
+//     if(err instanceof ZodError){
+//         const simplifiedError = handleZodError(err)
+//         console.log(simplifiedError)
+//         statusCode=simplifiedError?.statusCode,
+//     //    simplifiedError.errorDetails.map(el => {
+//     //     message=el.message;
+//     //    })
+//         message=simplifiedError.message,
+//         // errorDetails=simplifiedError?.errorDetails
+        
+//     }
+
+//     res.status(statusCode).json({
+//         success: false,
+//         message,
+//         // errorDetails
+//     })
+// };
+
+// export default globalErrorHandler;
 
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
---
+++ test.js
@@ -0,0 +1,16 @@
+{
+    "success": false,
+    "message": "Expected number, received string.Expected number, received string.",
+    "errorDetails": {
+        "issues": [
+            {
+                "field": "experience",
+                "message": "Expected number, received string"
+            },
+            {
+                "field": "appointFee",
+                "message": "Expected number, received string"
+            }
+        ]
+    }
+}
