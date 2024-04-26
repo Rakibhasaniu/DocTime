dc233aec6cd54ebeb27ba89e8039d085da96edec
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-04-26 16:07:57 +0600

    payment system finished

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -3,6 +3,7 @@ import config from '../../config';
 import prisma from '../../utils/prisma';
 import { SSlService } from '../ssl/ssl.service';
 import { PaymentStatus } from '@prisma/client';
+import { IPaymentData } from '../ssl/ssl.interface';
 
 const initPayment = async(id:string) => {
     const paymentData = await prisma.payment.findFirstOrThrow({
@@ -18,7 +19,7 @@ const initPayment = async(id:string) => {
         }
     })
     // console.log(paymentData)
-    const initPaymentData = {
+    const initPaymentData:IPaymentData = {
         amount: paymentData.amount,
         transactionId: paymentData.transactionId,
         name: paymentData.appointment.patient.name,
@@ -36,46 +37,46 @@ const initPayment = async(id:string) => {
     
 }
 const validatePayment = async (payload: any) => {
-    if (!payload || !payload.status || !(payload.status === 'VALID')) {
-        return {
-            message: "Invalid Payment!"
-        }
-    }
-
-    const response = await SSlService.validatePayment(payload);
-
-    if (response?.status !== 'VALID') {
-        return {
-            message: "Payment Failed!"
-        }
-    }
-
-    // const response = payload;
-
-    // await prisma.$transaction(async (tx) => {
-    //     const updatedPaymentData = await tx.payment.update({
-    //         where: {
-    //             transactionId: response.tran_id
-    //         },
-    //         data: {
-    //             status: PaymentStatus.PAID,
-    //             paymentGatewayData: response
-    //         }
-    //     });
-
-    //     await tx.appointment.update({
-    //         where: {
-    //             id: updatedPaymentData.appointmentId
-    //         },
-    //         data: {
-    //             paymentStatus: PaymentStatus.PAID
-    //         }
-    //     })
-    // });
-
-    // return {
-    //     message: "Payment success!"
+    // if (!payload || !payload.status || !(payload.status === 'VALID')) {
+    //     return {
+    //         message: "Invalid Payment!"
+    //     }
     // }
+
+    // const response = await SSlService.validatePayment(payload);
+
+    // if (response?.status !== 'VALID') {
+    //     return {
+    //         message: "Payment Failed!"
+    //     }
+    // }
+
+    const response = payload;
+
+    await prisma.$transaction(async (tx) => {
+        const updatedPaymentData = await tx.payment.update({
+            where: {
+                transactionId: response.tran_id
+            },
+            data: {
+                status: PaymentStatus.PAID,
+                paymentGatewayData: response
+            }
+        });
+
+        await tx.appointment.update({
+            where: {
+                id: updatedPaymentData.appointmentId
+            },
+            data: {
+                paymentStatus: PaymentStatus.PAID
+            }
+        })
+    });
+
+    return {
+        message: "Payment success!"
+    }
 
 }
 
